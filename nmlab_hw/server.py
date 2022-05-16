75c5e2f1ad0db2522b0c14293d16bf5bb93b3475
huai-ching <101869696+huai-ching@users.noreply.github.com>
2022-05-16 17:05:35 +0800

    Add files via upload

--- nmlab_hw/server.py
+++ nmlab_hw/server.py
@@ -10,6 +10,10 @@ from concurrent import futures
 import fib_pb2
 import fib_pb2_grpc
 
+import cv2
+import argparse
+import multiprocessing as mp
+
 
 class FibCalculatorServicer(fib_pb2_grpc.FibCalculatorServicer):
 
@@ -26,20 +30,7 @@ class FibCalculatorServicer(fib_pb2_grpc.FibCalculatorServicer):
         return response
 
     def _fibonacci(self, n):
-        a = 0
-        b = 1
-        if n < 0:
-            return 0
-        elif n == 0:
-            return 0
-        elif n == 1:
-            return b
-        else:
-            for i in range(1, n):
-                c = a + b
-                a = b
-                b = c
-            return b
+            return n
 
 
 if __name__ == "__main__":
@@ -59,3 +50,82 @@ if __name__ == "__main__":
         server.wait_for_termination()
     except KeyboardInterrupt:
         pass
+
+def gstreamer_camera(queue):
+    # Use the provided pipeline to construct the video capture in opencv
+    pipeline = (
+        "nvarguscamerasrc ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)1920, height=(int)1080, "
+            "format=(string)NV12, framerate=(fraction)30/1 ! "
+        "queue ! "
+        "nvvidconv flip-method=2 ! "
+            "video/x-raw, "
+            "width=(int)1920, height=(int)1080, "
+            "format=(string)BGRx, framerate=(fraction)30/1 ! "
+        "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! "
+        "appsink"
+    )
+
+    cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+    try:
+        while True:
+            # print('producer')
+            ret, frame = cap.read()
+            if not ret:
+                break
+            # if queue.full():
+            #     print('loss')
+            queue.put(frame)
+            # print(time.strftime('%X'), frame.shape)
+    except KeyboardInterrupt as e:
+        cap.release()
+
+    # Complete the function body
+    pass
+
+
+def gstreamer_rtmpstream(queue):
+    # Use the provided pipeline to construct the video writer in opencv
+    pipeline = (
+        "appsrc ! "
+            "video/x-raw, format=(string)BGR ! "
+        "queue ! "
+        "videoconvert ! "
+            "video/x-raw, format=RGBA ! "
+        "nvvidconv ! "
+        "nvv4l2h264enc bitrate=8000000 ! "
+        "h264parse ! "
+        "flvmux ! "
+        'rtmpsink location="rtmp://localhost/rtmp/live live=1"'
+    )
+
+    out = cv2.VideoWriter(pipeline, cv2.CAP_GSTREAMER, 30.0, (1920, 1080))
+    try:
+        while True:
+            if not queue.empty():
+                # print('consumer')
+                f = queue.get()
+		
+                out.write(f)
+    except KeyboardInterrupt as e:
+        # cap.release()
+        pass
+   
+
+    # Complete the function body
+    
+    # You can apply some simple computer vision algorithm here
+    pass
+
+
+queue = mp.Queue(maxsize=10)
+
+p1 = mp.Process(target=gstreamer_camera, args=(queue, ))
+p2 = mp.Process(target=gstreamer_rtmpstream, args=(queue, ))
+
+p1.start()
+p2.start()
+p1.join()
+p2.join()
